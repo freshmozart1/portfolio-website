e0a7e7e6313a63ddcb4e5b0bcef4c4f3810b5617
freshmozart1 <78731534+freshmozart1@users.noreply.github.com>
2024-05-13 09:18:15 +0000

    I followed a YouTube video here. This might be a solution for pagination in the future.

--- playground/js/scripts.js
+++ playground/js/scripts.js
@@ -1,39 +1,28 @@
-var css_ids = createCSSidList();
-
 window.onload = () => {
-    console.log(css_ids);
-    animateLabelSwitch('email_input', 'valid_email_label', 'invalid_email_label');
-}
-
-function createCSSidList() {
-    let list_of_ids = [];
-    const html_nodes_with_ids = document.querySelectorAll('*[id]');
-    html_nodes_with_ids.forEach((currentValue) => {
-        list_of_ids.push(currentValue.id);
-    });
-    return list_of_ids;
-}
-
-function animateLabelSwitch(inputId, valid_label_id, invalid_label_id) {
-    if (css_ids.includes(inputId) && css_ids.includes(valid_label_id) && css_ids.includes(invalid_label_id)) {
-        const inputFormElement = document.getElementById(inputId);
-        const valid_label = document.getElementById(valid_label_id);
-        const invalid_label = document.getElementById(invalid_label_id);
-        inputFormElement.addEventListener('input', (event) => {
-            if (event.target.value) {
-                if (inputFormElement.checkValidity()) {
-                    valid_label.style.animation = '0.3s ease 0s 1 forwards running fade-in';
-                    invalid_label.style.animation = '0.3s ease 0s 1 forwards running fade-out';
-                } else {
-                    valid_label.style.animation = '0.3s ease 0s 1 forwards running fade-out';
-                    invalid_label.style.animation = '0.3s ease 0s 1 forwards running fade-in';
-                }
-            } else {
-                valid_label.style.animation = '0.3s ease 0s 1 forwards running fade-in';
-                invalid_label.style.animation = '0.3s ease 0s 1 forwards running fade-out';
-            }
+    const slider_nav = Array.from(document.getElementsByClassName('slider-nav'));
+    const slider_nav_dots = Array.from(slider_nav[0].getElementsByTagName('a'));
+    slider_pictures = Array.from(document.getElementsByTagName('img'));
+    let slides = [];
+    slider_nav_dots.forEach((dot, index) => {
+        dot.addEventListener('click', () => {
+            changeActiveSlide(slides, index);
         });
-    } else {
-        console.error('One or more of the provided ids do not exist in the HTML document.');
-    }
+        slides.push({
+            dot: dot,
+            img: slider_pictures[index]
+        });
+    });
+};
+
+function changeActiveSlide(slides, index) {
+    slides.forEach((slide, i) => {
+        if (i === index) {
+            slide.dot.classList.add('active');
+            slide.dot.classList.remove('inactive');
+        } else {
+            slide.dot.classList.remove('active');
+            slide.dot.classList.add('inactive');
+        }
+    });
+
 }
